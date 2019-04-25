b7f2316ad3668816c0ed2401324d8d73315dc36e
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-25 15:57:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    new_sentence = self.split(/.?!/)
+    new_sentence.to_a 
+    new_sentence.count
+
 
   end
+  #binding.pry
 end
